3bf230185345af4475776bc02bb994bf2f79e277
Joshua <horheyinc@gmail.com>
2024-11-11 11:50:40 +0100

    react day one

---
+++ module-2/week1/day1/first-vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Vite Project</h1>
+      <img src={viteLogo} alt="vite logo" />
+    </>
+  );
+}
+
+export default App;
