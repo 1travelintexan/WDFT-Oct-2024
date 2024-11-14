9dd6367f6957183abcf8e06d86af6579f49c3fa7
Joshua <horheyinc@gmail.com>
2024-11-14 12:31:59 +0100

    routing

--- module-2/week1/day4/routing/src/components/AboutPage.jsx
+++ module-2/week1/day4/routing/src/components/AboutPage.jsx
@@ -1,10 +1,12 @@
 import { useSearchParams } from "react-router-dom";
-
 const AboutPage = () => {
+  //the useSearchParams returns and array with the queries inside index 0
   const [theQueries] = useSearchParams();
+  //after you get the return from useSearchParams you have to call the .get()
   const theTestQuery = theQueries.get("test");
   const thePizzaQuery = theQueries.get("test2");
   console.log(theTestQuery, thePizzaQuery);
+
   return <div>AboutPage</div>;
 };
 export default AboutPage;
