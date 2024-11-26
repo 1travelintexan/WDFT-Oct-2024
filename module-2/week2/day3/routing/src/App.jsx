7e2c259e0a10cb1c6d08a3d61eda34b3a7c35276
Joshua <horheyinc@gmail.com>
2024-11-26 12:45:20 +0100

    outlet components and spinners

--- module-2/week2/day3/routing/src/App.jsx
+++ module-2/week2/day3/routing/src/App.jsx
@@ -1,23 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import UserListPage from "./pages/UserListPage";
 import UserDetailPage from "./pages/UserDetailPage";
 import AddUserPage from "./pages/AddUserPage";
-import Navbar from "./components/Navbar";
 import UpdateUser from "./pages/UpdateUser";
+import MyOutlet from "./components/MyOutlet";
+import Spinner from "react-bootstrap/Spinner";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  if (loading) {
+    return <Spinner animation="grow" variant="info" />;
+  }
   return (
     <>
-      <Navbar />
-      <h1>Axios Day</h1>
       <main>
         <Routes>
           <Route path="/" element={<UserListPage />} />
-          <Route path="/user/detail/:userId" element={<UserDetailPage />} />
+          <Route
+            path="/user/detail/:userId"
+            element={
+              <MyOutlet>
+                <UserDetailPage />
+              </MyOutlet>
+            }
+          />
           <Route path="/user/update/:userId" element={<UpdateUser />} />
-          <Route path="/add-user" element={<AddUserPage />} />
+          <Route
+            path="/add-user"
+            element={
+              <MyOutlet>
+                <AddUserPage />
+              </MyOutlet>
+            }
+          />
         </Routes>
       </main>
     </>
--- module-2/week2/day3/routing/src/main.jsx
+++ module-2/week2/day3/routing/src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import { StrictMode } from "react";
 
 createRoot(document.getElementById("root")).render(
-  <Router>
-    <App />
-  </Router>
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
 );
