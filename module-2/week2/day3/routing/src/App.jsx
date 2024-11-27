bc1f41281a7c1af1a1ec03aa1e04c1d1f8e6c5ba
Joshua <horheyinc@gmail.com>
2024-11-27 11:13:05 +0100

    image upload

--- module-2/week2/day3/routing/src/App.jsx
+++ module-2/week2/day3/routing/src/App.jsx
@@ -7,14 +7,30 @@ import AddUserPage from "./pages/AddUserPage";
 import UpdateUser from "./pages/UpdateUser";
 import MyOutlet from "./components/MyOutlet";
 import Spinner from "react-bootstrap/Spinner";
+import axios from "axios";
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [image, setImage] = useState();
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 3000);
   }, []);
+
+  //functions
+  function handleAddImage(e) {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("file", image);
+    formData.append("upload_present", "IronhackWD");
+    axios
+      .post("https://api.cloudinary.com/v1_1/dxurcuyga/image/upload", formData)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }
 
   if (loading) {
     return <Spinner animation="grow" variant="info" />;
@@ -22,6 +38,15 @@ function App() {
   return (
     <>
       <main>
+        <form onSubmit={handleAddImage}>
+          <input
+            type="file"
+            onChange={(e) => {
+              setImage(e.target.files[0]);
+            }}
+          />
+          <button>Add Image</button>
+        </form>
         <Routes>
           <Route path="/" element={<UserListPage />} />
           <Route
