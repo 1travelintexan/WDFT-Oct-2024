2442901010eabae96655f39a3e777c397526432e
Joshua <horheyinc@gmail.com>
2025-01-09 09:16:37 +0100

    utility types

--- module-4/day5/src/index.ts
+++ module-4/day5/src/index.ts
@@ -1,5 +1,7 @@
 const people = ["Malek", "Joshua", "Bruno", "Stephan"];
 const numbers = [1, 3, 4, 5, 77];
+
+console.log("hello");
 //*******generics **********/
 function getRandom<T>(arr: T[]) {
   const randomIndex = Math.floor(Math.random() * arr.length);
@@ -26,3 +28,32 @@ const Ragnar: person<number> = {
   name: "Ragni",
   age: 3,
 };
+
+//**************Utility types ******/
+//make every property optional
+type user = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+type userOptional = Partial<user>;
+
+//make every property required
+type userOptional2 = {
+  name?: string;
+  email: string;
+  password?: string;
+};
+type userRequired = Required<userOptional2>;
+
+//remove properties that you do NOT want
+type bigUser2 = {
+  name: string;
+  email: string;
+  password: string;
+  LastName: string;
+  age: number;
+  phoneNumber: string;
+};
+type smallUser2 = Omit<bigUser2, "phoneNumber" | "email" | "password">;
